117eb381b6761dce168eefcf82c43dabc9b75a7c
hoiku <103164926+hoiku@users.noreply.github.com>
2025-06-18 12:47:00 +0900

    Update vite.config.js
    "fix: ensure env variables injected in build"

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  base: '/kport-anno/',
-  build: {
-    outDir: 'dist'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) // .env 값 로딩
+
+  return {
+    base: '/kport-anno/',
+    build: {
+      outDir: 'dist'
+    },
+    define: {
+      'import.meta.env': env
+    }
   }
-});
+})
