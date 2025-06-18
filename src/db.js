993217c305bc8b2a0b72ef11247296edc18e40d6
hoiku <103164926+hoiku@users.noreply.github.com>
2025-06-18 17:19:01 +0900

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+console.log('▶ SUPABASE_URL =', SUPABASE_URL);
+console.log('▶ SUPABASE_KEY =', SUPABASE_KEY);
 
-console.log('URL:', SUPABASE_URL);
-console.log('Key:', SUPABASE_KEY);
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
