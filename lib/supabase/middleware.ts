39d88b3eb0404bf2f8360a5bb9bc8fc1572cf3ba
hoiku <103164926+hoiku@users.noreply.github.com>
2025-07-21 12:44:50 +0900

    Update middleware.ts
    Update middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,14 +1,15 @@
-import { authMiddleware } from "@supabase/auth-helpers-nextjs";
 
-// 인증이 필요한 경로를 지정
+import { updateSession } from "@/lib/supabase/middleware";
+import { type NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
 export const config = {
   matcher: [
-    '/app/adm/:path*',   // 관리자 페이지 전용
-    '/app/user/:path*',  // 사용자 페이지 전용
+    '/app/adm/:path*',
+    '/app/user/:path*',
+    '/dashboard/:path*',
   ],
 };
-
-// 인증되지 않은 사용자는 자동으로 이 경로로 리다이렉트
-export default authMiddleware({
-  redirectTo: '/auth/login',
-});
