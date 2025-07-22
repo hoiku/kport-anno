7ed6ff4a687ce0b1b1e057a08a4d0c97b3a03364
hoiku <103164926+hoiku@users.noreply.github.com>
2025-07-22 12:46:05 +0900

    Update middleware.ts
    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,28 @@
-import { protectRoutes } from '@/lib/supabase/middleware'
-import { type NextRequest } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
+  const response = NextResponse.next()
+  const supabase = createMiddlewareClient({ req: request, res: response })
 
-  // 로그인 페이지는 인증 없이 접근 가능
-  if (pathname.startsWith('/auth/login')) {
-    return
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // 세션 없으면 로그인으로 리다이렉트
+  if (!session && request.nextUrl.pathname.startsWith('/app')) {
+    return NextResponse.redirect(new URL('/auth/login', request.url))
   }
 
-  return await protectRoutes(request)
+  return response
 }
 
 export const config = {
   matcher: [
-    // 보호 대상 경로
+    // 보호할 경로
     '/app/adm/:path*',
     '/app/user/:path*',
-
-    // Next.js 내부 파일, 이미지, favicon 등 제외
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    // 정적 리소스 제외
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
