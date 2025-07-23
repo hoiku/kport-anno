7c32e22002a31102ca5dacc48f27cc8760699478
hoiku <103164926+hoiku@users.noreply.github.com>
2025-07-23 19:38:03 +0900

    Update uploadImg.tsx
    Update uploadImg

--- components/adm/uploadImg.tsx
+++ components/adm/uploadImg.tsx
@@ -3,7 +3,10 @@
 import { useState } from 'react'
 import { createBrowserClient } from '@supabase/ssr'
 
-const supabase = createBrowserClient()
+const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
 
 export default function UploadImage() {
   const [file, setFile] = useState<File | null>(null)
