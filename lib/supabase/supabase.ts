0bb4ee6faef7d90ad8ba6621ff69c94d436ea551
hoiku <103164926+hoiku@users.noreply.github.com>
2025-07-30 20:55:55 +0900

    Update supabase.ts
    Update supabase.

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+// ✅ 전역 인스턴스를 재사용
+export const supabase =
+  globalThis.supabase ||
+  createClient(supabaseUrl, supabaseAnonKey)
+
+if (typeof window !== 'undefined') {
+  globalThis.supabase = supabase
+}
